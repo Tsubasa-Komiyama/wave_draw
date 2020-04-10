b07c08c9c1813f211eb8bd379824eecc4d647f75
Tsubasa-Komiyama <komiyama-tsubasa-vw@ynu.jp>
2020-04-10 13:10:23 +0900

    change resource.h

--- resource.h
+++ resource.h
@@ -1,23 +1,16 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ ‚Å¶¬‚³‚ê‚½ƒCƒ“ƒNƒ‹[ƒh ƒtƒ@ƒCƒ‹B
-// resource.rc ‚Åg—p
+// Microsoft Visual C++ ï¿½Åï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ê‚½ï¿½Cï¿½ï¿½ï¿½Nï¿½ï¿½ï¿½[ï¿½h ï¿½tï¿½@ï¿½Cï¿½ï¿½ï¿½B
+// resource.rc ï¿½Ågï¿½p
 //
 #define IDD_MAINDIALOG                  101
-#define IDC_PICTBOX                     1000
 #define ID_PICTBOX1                     1000
-#define ID_OK                           1001
-#define IDC_PICTBOX2                    1001
 #define ID_PICTBOX2                     1001
 #define ID_END                          1002
-#define IDC_RADIO1                      1005
-#define IDC_RADIO2                      1006
 #define ID_START                        1006
-#define IDC_RADIO3                      1007
 #define ID_STOP                         1007
-#define ID_RESTART                      1008
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
